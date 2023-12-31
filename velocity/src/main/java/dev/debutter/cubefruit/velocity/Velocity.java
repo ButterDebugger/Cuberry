3ea894865144148fd550fffa3cc952353f41ab38
ButterBoyJulian <34288129+ButterDebugger@users.noreply.github.com>
2023-12-31 01:21:33 -0500

    Updated project version

--- velocity/src/main/java/dev/debutter/cubefruit/velocity/Velocity.java
+++ velocity/src/main/java/dev/debutter/cubefruit/velocity/Velocity.java
@@ -25,7 +25,7 @@ import java.util.Optional;
         id = "cubefruit",
         name = "CubeFruit",
         authors = "ButterDebugger",
-        version = "1.10.1"
+        version = "1.10.2"
 )
 public class Velocity {
 
